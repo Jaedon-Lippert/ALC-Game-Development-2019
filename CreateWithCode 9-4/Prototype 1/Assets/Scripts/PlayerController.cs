0a57ca2c8b28508cd51fc86fa7ed485ce6b7974e
Jaedon Lippert <jaedon.l293@stu.nebo.edu>
2019-09-18 10:29:53 -0600

    Vehicle Movements
    Left Right pivot
    Forward Backward movement
    Click to Boost

--- CreateWithCode 9-4/Prototype 1/Assets/Scripts/PlayerController.cs
+++ CreateWithCode 9-4/Prototype 1/Assets/Scripts/PlayerController.cs
@@ -6,6 +6,12 @@ public class PlayerController : MonoBehaviour
 {
     //Public Variables
     public float speed = 20f;
+    public float turnSpeed;
+    public float horizontalInput;
+    public float verticalInput;
+
+    public float boostInput;
+    public float boostSpeed;
 
     // Start is called before the first frame update
     void Start()
@@ -16,7 +22,19 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //Button Presses
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
+
+        //BOOST
+        boostInput = Input.GetAxis("Fire1");
+
         //Move Forward
-        transform.Translate(Vector3.forward * Time.deltaTime * speed);
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * verticalInput * (1 + (boostInput * boostSpeed)));
+
+        //Rotate
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        //Move Right
+        //transform.Translate(Vector3.right * Time.deltaTime * turnSpeed * horizontalInput)
     }
 }
