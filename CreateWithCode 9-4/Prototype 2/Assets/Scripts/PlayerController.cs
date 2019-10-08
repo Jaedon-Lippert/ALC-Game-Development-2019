863de6a2a0b12f1c5d2feed6eedd4082d6973c19
Jaedon Lippert <jaedon.l293@stu.nebo.edu>
2019-10-08 10:29:30 -0600

    Complete lesson 2.1
    Completed Lesson 2.1
    Created Projectile and Animal Movements

---
+++ CreateWithCode 9-4/Prototype 2/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    //Movement Variables
+    public float horizontalInput;
+    public float speed = 10.0f;
+    //Boundary Variable
+    public float xRange = 15.0f;
+
+    //Projectile
+    public GameObject projectilePrefab;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //--Movement--//
+        //Left and Right Input
+        horizontalInput = Input.GetAxis("Horizontal");
+        //Move left and right
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //--Movement Limiter--//
+        //Prevent player from going passed -xRange
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+        //Prevent player from going passed xRange
+        if (transform.position.x > xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
+
+        //--Shoot Projectile--//
+        //If space bar is pressed
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Fire projectilePrefab at PlayerPosition
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        //If key s is held
+        /*
+        if (Input.GetKey(KeyCode.S))
+        {
+            //Fire projectilePrefab at PlayerPosition
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
+        */
+    }
+}
