842e55bac8bba1acb39b603245ff47ae3deabaa2
Jaedon Lippert <jaedon.l293@stu.nebo.edu>
2019-10-16 10:25:00 -0600

    //SpawnManager and Colliders

--- CreateWithCode 9-4/Prototype 2/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode 9-4/Prototype 2/Assets/Scripts/SpawnManager.cs
@@ -4,25 +4,51 @@ using UnityEngine;
 
 public class SpawnManager : MonoBehaviour
 {
+    //Array
     public GameObject[] animalPrefabs = new GameObject[3];
-    public int animalIndex;
-    public int xPos;
+
+    //Location
+    public float xPos;
+    public float xRange = 15;
+    public float zLocation = 20;
+
+    //Randomizer
     System.Random rSeed;
+
+    //Timer
+    private float startDelay = 2f;
+    private float spawnInterval= 1.5f;
+
     // Start is called before the first frame update
     void Start()
     {
+        //Creates new randomizer seed to prevent duplication of sequences
         rSeed = new System.Random();
+
+        //Timed Intervals
+        InvokeRepeating("SpawnRandomAnimal", startDelay, spawnInterval);
+        
     }
 
     // Update is called once per frame
     void Update()
     {
-        animalIndex = rSeed.Next(0, animalPrefabs.Length);
-        xPos = rSeed.Next(-20,20);
-
-        if (Input.GetKey(KeyCode.S))
+        /*
+        if (Input.GetKeyDown(KeyCode.S))
         {
-            Instantiate(animalPrefabs[animalIndex], new Vector3(xPos, 0, 20), animalPrefabs[animalIndex].transform.rotation);
+            SpawnRandomAnimal();
         }
+        */
+    }
+
+    void SpawnRandomAnimal()
+    {
+        //Selects a random animal from array
+        int animalIndex = rSeed.Next(0, animalPrefabs.Length);
+        //Random position within xRange
+        xPos = ((float)rSeed.NextDouble() * 2 * xRange) - xRange;
+
+        //Instantiates animal prefab
+        Instantiate(animalPrefabs[animalIndex], new Vector3(xPos, 0, zLocation), animalPrefabs[animalIndex].transform.rotation);
     }
 }
