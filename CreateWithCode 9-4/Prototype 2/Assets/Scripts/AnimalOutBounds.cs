a2a50ca769ad218c7b6222ece90781a95dee190d
Jaedon Lippert <jaedon.l293@stu.nebo.edu>
2019-10-22 10:16:08 -0600

    Finished 2.2

--- CreateWithCode 9-4/Prototype 2/Assets/Scripts/AnimalOutBounds.cs
+++ CreateWithCode 9-4/Prototype 2/Assets/Scripts/AnimalOutBounds.cs
@@ -2,21 +2,26 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class DestoyOutOfBounds : MonoBehaviour
+public class AnimalOutBounds : MonoBehaviour
 {
     public float front_bound = 35.0f;
     public float back_bound = -15.0f;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (back_bound > transform.position.z || transform.position.z > front_bound)
+        if (transform.position.z > front_bound)
         {
+            Destroy(gameObject);
+        }
+        else if (back_bound > transform.position.z)
+        {
+            Debug.Log("Game Over!");
             Destroy(gameObject);
         }
     }
