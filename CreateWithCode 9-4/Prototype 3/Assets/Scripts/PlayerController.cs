85165e5f8896b78c0ed981e63eab535761c70e3e
Jaedon Lippert <jaedon.l293@stu.nebo.edu>
2019-11-11 10:09:16 -0700

    //Moving Background and Gamover

--- CreateWithCode 9-4/Prototype 3/Assets/Scripts/PlayerController.cs
+++ CreateWithCode 9-4/Prototype 3/Assets/Scripts/PlayerController.cs
@@ -9,6 +9,7 @@ public class PlayerController : MonoBehaviour
     public float gravityModifier;
     public bool isOnGround = true;
     public bool isBlocked = false;
+    public bool gameOver = false;
 
     // Start is called before the first frame update
     void Start()
@@ -36,7 +37,14 @@ public class PlayerController : MonoBehaviour
 
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if(collision.gameObject.CompareTag("Obstacle")){
+            Debug.Log("Game Over");
+            gameOver = true;
+        }
     }
 
     private void OnTriggerEnter(Collider other)
--- CreateWithCode 9-4/Prototype 3/Assets/Scripts/SpawnManager.cs
+++ CreateWithCode 9-4/Prototype 3/Assets/Scripts/SpawnManager.cs
@@ -6,10 +6,12 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] obstaclePrefabs;
     private Vector3 spawnPos = new Vector3(25, 0, 0);
+    private PlayerController playerControllerScript;
     // Start is called before the first frame update
     void Start()
     {
-        InvokeRepeating("SpawnObstacle", 0.5f, 0.75f);
+        InvokeRepeating("SpawnObstacle", 0.85f, 0.75f);
+        playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
 
     // Update is called once per frame
@@ -20,7 +22,10 @@ public class SpawnManager : MonoBehaviour
 
     void SpawnObstacle()
     {
-        int rand = Random.Range(0, obstaclePrefabs.Length);
-        Instantiate(obstaclePrefabs[rand], spawnPos, obstaclePrefabs[rand].transform.rotation);
+        if (!playerControllerScript.gameOver)
+        {
+            int rand = Random.Range(0, obstaclePrefabs.Length);
+            Instantiate(obstaclePrefabs[rand], spawnPos, obstaclePrefabs[rand].transform.rotation);
+        }
     }
 }
